eb5f9b5e9eefbb02fa0c76e8fec7cbf2a5454177
Aleksei Sobolevskii <sobolevsky.alexey@gmail.com>
2022-06-17 22:30:28 -0400

    Initial Commit

---
+++ FootballFeed/FootballFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballFeedApp.swift
+//  FootballFeed
+//
+//  Created by Aleksei Sobolevskii on 2022-06-17.
+//
+
+import SwiftUI
+
+@main
+struct FootballFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
